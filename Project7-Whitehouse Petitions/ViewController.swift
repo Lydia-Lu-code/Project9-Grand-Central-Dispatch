6233b4a033d5ff94799502dbca48f12f723f5457
Lydia <guipopo5@gmail.com>
2024-02-26 19:33:56 +0800

    Initial Commit

---
+++ Project7-Whitehouse Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7-Whitehouse Petitions
+//
+//  Created by Lydia Lu on 2024/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
