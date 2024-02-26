227f2c151fd21e30f43c77a170184c605718bf15
Lydia <guipopo5@gmail.com>
2024-02-26 20:26:10 +0800

    OK

---
+++ Project7-Whitehouse Petitions/Petition.swift
@@ -0,0 +1,8 @@
+//
+//  Petition.swift
+//  Project7-Whitehouse Petitions
+//
+//  Created by Lydia Lu on 2024/2/26.
+//
+
+import Foundation
---
+++ Project7-Whitehouse Petitions/Petitions.swift
@@ -0,0 +1,8 @@
+//
+//  Petitions.swift
+//  Project7-Whitehouse Petitions
+//
+//  Created by Lydia Lu on 2024/2/26.
+//
+
+import Foundation
